9e9a6687a3c3893d62d9a5fe9955bf9b23548cb1
busterwood <chris.j.austin@gmail.com>
2015-09-21 14:00:49 +0100

    Better CPU stats in benchmarks

--- UnitTests/Benchmark.cs
+++ UnitTests/Benchmark.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using FILETIME = System.Runtime.InteropServices.ComTypes.FILETIME;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 namespace NetChan {
     class Benchmark {
@@ -31,7 +34,7 @@ namespace NetChan {
 		        n = roundUp(n);
 		        b.RunN(n, code);
 	        }
-            Console.WriteLine("{4}: {0:#,##0}, {1:0.00} ns/op, {2:N0}MS CPU, {3:N0} memory", b.N, b.NsPerOp(), b.totalCPU.TotalMilliseconds, b.totalMemory, name);
+            Console.WriteLine("{4}: {0:#,##0}, {1:0.00} ns/op, {2:0.00} User, {3:0.00} Kernel", b.N, b.NsPerOp(), b.totalCPU.User.TotalSeconds, b.totalCPU.Kernel.TotalSeconds, name);
         }
 
         static int Max(int x, int y) {
@@ -72,25 +75,26 @@ namespace NetChan {
         }
 
         internal class B {
+         
             private Stopwatch sw = new Stopwatch();
             public int N;
             private long totalTicks;
             private long startMemory;
             public long totalMemory;
-            private TimeSpan startCPU;
-            public TimeSpan totalCPU;
+            private CpuTime startCPU;
+            public CpuTime totalCPU;
             
             public void Start() {
                 if (sw.IsRunning) return;
                 startMemory = GC.GetTotalMemory(true);
-                startCPU = Process.GetCurrentProcess().TotalProcessorTime;
+                startCPU = NativeMethods.GetProcessTime();
                 sw.Start();
             }
 
             public void Stop() {
                 if (!sw.IsRunning) return;
                 sw.Stop();
-                totalCPU += Process.GetCurrentProcess().TotalProcessorTime - startCPU;
+                totalCPU += NativeMethods.GetProcessTime() - startCPU;
                 totalMemory += GC.GetTotalMemory(true) - startMemory;
                 totalTicks += sw.ElapsedTicks;
             }
@@ -119,6 +123,44 @@ namespace NetChan {
                     Stop();
                 }
             }
+        }
+    }
+
+    internal static class NativeMethods {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetProcessTimes(IntPtr hProcess, out FILETIME creation, out FILETIME exit, out FILETIME kernel, out FILETIME user);
+
+        public static TimeSpan AsTimeSpan(this FILETIME fileTime) {
+            //NB! uint conversion must be done on both fields before ulong conversion
+            ulong hFT2 = unchecked((((ulong)(uint)fileTime.dwHighDateTime) << 32) | (uint)fileTime.dwLowDateTime);
+            return TimeSpan.FromTicks((long)hFT2);
+        }
+
+        public static CpuTime GetProcessTime() {
+            FILETIME creation, exit, kernel, user;
+            if (!GetProcessTimes(Process.GetCurrentProcess().Handle, out creation, out exit, out kernel, out user)) {
+                throw new Win32Exception();
+            }
+            return new CpuTime(kernel.AsTimeSpan(), user.AsTimeSpan());
+        }
+    }
+
+    struct CpuTime {
+        public readonly TimeSpan Kernel;
+        public readonly TimeSpan User;
+
+        public CpuTime(TimeSpan kernel, TimeSpan user) {
+            Kernel = kernel;
+            User = user;
+        }
+
+        public static CpuTime operator +(CpuTime x, CpuTime y) {
+            return new CpuTime(x.Kernel + y.Kernel, x.User + y.User);
+        }
+
+        public static CpuTime operator -(CpuTime x, CpuTime y) {
+            return new CpuTime(x.Kernel - y.Kernel, x.User - y.User);
         }
     }
 }
